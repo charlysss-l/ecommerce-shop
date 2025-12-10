223b314cf4d9048cdb32a3b6661276c5f5258ba1
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2025-12-10 10:19:47 +0800

    Add image in favorites page

--- my-shop/pages/shop/favorites.js
+++ my-shop/pages/shop/favorites.js
@@ -1,6 +1,7 @@
 import { useCustomer } from '../../context/CustomerContext';
 import CustomerNavbar from '../../components/CustomerNavbar';
 import ProtectedCustomer from '../../components/ProtectedCustomer';
+
 export default function FavoritesPage() {
   const { favorites, removeFromFavorites } = useCustomer();
 
@@ -11,10 +12,22 @@ export default function FavoritesPage() {
         <h1>My Favorites</h1>
         {favorites.length === 0 && <p>No favorite products.</p>}
         {favorites.map(p => (
-          <div key={p._id} style={{ borderBottom: '1px solid #ccc', marginBottom: 10, paddingBottom: 10 }}>
+          <div
+            key={p._id}
+            style={{ borderBottom: '1px solid #ccc', marginBottom: 10, paddingBottom: 10 }}
+          >
+            {p.image && (
+              <img
+                src={p.image}
+                alt={p.title}
+                style={{ width: 100, height: 100, objectFit: 'cover', marginBottom: 5 }}
+              />
+            )}
             <p><strong>{p.title}</strong></p>
             <p>Price: ${p.price}</p>
-            <button onClick={() => removeFromFavorites(p._id)}>Remove from Favorites</button>
+            <button onClick={() => removeFromFavorites(p._id)}>
+              Remove from Favorites
+            </button>
           </div>
         ))}
       </div>
